bc17b8d6730f4a2daeee65cf16335dd0a7e131e1
Jose Montero Vargas <fdo2494montero@gmail.com>
2016-03-17 01:16:46 -0600

    PrimerCommit

--- src/Controlador/Controlador_Mantenimiento_Estudiante.java
+++ src/Controlador/Controlador_Mantenimiento_Estudiante.java
@@ -5,9 +5,12 @@
  */
 package Controlador;
 
+import Modelo.ArrayEstudiante;
+import Modelo.Estudiante;
 import Vista.ManipulaEstudiantes;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -23,8 +26,11 @@ public class Controlador_Mantenimiento_Estudiante implements ActionListener {
     public final static int MODIFICAR = 6;
     public final static int BUSCAR = 7;
     
+    private String carnet, correo, nombre;
     
     private ManipulaEstudiantes manipulaEstudiantes;
+    private Estudiante estudiante;
+    private ArrayEstudiante arrayEstudiante;
     
     public Controlador_Mantenimiento_Estudiante(ManipulaEstudiantes manipulaEstudiantes) {
         this.manipulaEstudiantes = manipulaEstudiantes;       
@@ -39,8 +45,44 @@ public class Controlador_Mantenimiento_Estudiante implements ActionListener {
         switch(codigo)
         {
             case AGREGAR:
+                carnet = jtCarnet.getText();
+                correo = jtCorreo.getText();
+                nombre = jtNombre.getText();
+                
+                estudiante = new Estudiante(carnet, nombre, correo);
+                arrayEstudiante = new ArrayEstudiante();
+                arrayEstudiante.agregarEstudiante(estudiante);
+                
             case ELIMINAR:
+                if(!jtCarnet.getText().equals(""))
+                {
+                        arrayEstudiante.eliminarEstudiante(jtCarnet.getText());
+                }
+                else
+                {
+                        JOptionPane.showMessageDialog(null,"Digite un Carnet");
+                }
+                
             case BUSCAR:
+                if(!jtCarnet.getText().equals(""))
+                {
+                    arrayEstudiante.buscarEstudiante(jtCarnet.getText());
+                    if(arrayEstudiante.buscarEstudiante(jtCarnet.getText()) != false)
+                    {
+                            jtCarnet.setText(estudiante.getCarnet());
+                            jtCorreo.setText(estudiante.getCorreo());
+                            jtNombre.setText(estudiante.getNombre());
+                            
+                    }
+                    else
+                    {
+                            JOptionPane.showMessageDialog(null,"El estudiante no se encuentra");
+                    }
+                }
+                else
+                {
+                        JOptionPane.showMessageDialog(null,"Digite un carnet");
+                }
             case MODIFICAR:
         }
     }
--- src/Modelo/ArrayEstudiante.java
+++ src/Modelo/ArrayEstudiante.java
@@ -6,6 +6,7 @@
 package Modelo;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 /**
  *
  * @author DELL
@@ -22,12 +23,12 @@ public class ArrayEstudiante
         lista = new ArrayList<Estudiante>();
     }
     
-    public void agregar(Estudiante estudiante)
+    public void agregarEstudiante(Estudiante estudiante)
     {
         lista.add(estudiante);
     }
     
-    public Estudiante buscar(String carnet)
+    public Estudiante buscarEstudiante(String carnet)
     {
         Estudiante estudiante = null;
          for(int i = 0; i < lista.size(); i++)
@@ -38,6 +39,31 @@ public class ArrayEstudiante
              }
          }
          return estudiante;
+    }
+    
+    public void eliminarEstudiante(String carnet)
+    {
+        int opcion = 2;
+        for(int i = 0; i < lista.size(); i++)
+        {
+                if(lista.get(i).getCarnet().equals(carnet))
+                {
+                        opcion = Integer.parseInt(JOptionPane.showInputDialog("Esta seguro que desea eliminar al estudiante? \n\n      1. SI    2. NO"));
+                        if(opcion == 1)
+                        {
+                                lista.remove(lista.get(i));
+                                JOptionPane.showMessageDialog(null,"El estudiante se elimino correctamente");
+                        }
+                        else
+                        {
+                                JOptionPane.showMessageDialog(null,"El estudiante NO se elimino");
+                        }
+                }
+                else
+                {
+                        JOptionPane.showMessageDialog(null,"El estudiante no se encuentra");
+                }
+        }
     }
 }
 
